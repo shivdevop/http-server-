210e4e73f89bd10aeb4021ffb533130d98b62cdb
1000121940 <shivamagarwalla05@gmail.com>
2025-01-21 14:56:20 +0530

    added body parser

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express=require('express');
+const bodyParser = require('body-parser');
 const app=express();
 const port=3000;
-
+app.use(bodyParser.json());
 app.get('/',(req,res)=>{
     res.send('Hello  hi there');
 });
+
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    res.json({
+        msg: 'Hello from the server',
+    })
+})
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
