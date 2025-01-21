b45cdeceb458bf914af7e7e0d49c7bcb1ae552d7
1000121940 <shivamagarwalla05@gmail.com>
2025-01-21 15:12:51 +0530

    body parser and env variables and gitignore

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express=require('express');
-const app=express();
-const port=3000;
+const bodyParser = require('body-parser');
 
+//Load environment variables from .env file
+require('dotenv').config();
+
+const app=express();
+
+
+const port=process.env.PORT;
+const secretKey=process.env.SECRETKEY;
+
+app.use(bodyParser.json());
 app.get('/',(req,res)=>{
     res.send('Hello  hi there');
 });
 
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    res.json({
+        msg: 'Hello from the server',
+    })
+})
+
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
+    console.log(`Secret key is ${secretKey}`);
 });
